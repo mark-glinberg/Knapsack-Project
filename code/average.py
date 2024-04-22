d196489690c630ac580eeadf0853fb580fc5308e
san98215 <s-nye@comcast.net>
2024-04-22 14:39:55 -0400

    Updated average.py and added avg trace files

--- code/average.py
+++ code/average.py
@@ -1,6 +1,6 @@
 import glob
 
-alg = "ls2" # Modify for ls1 or ls2
+alg = "ls1" # Modify for ls1 or ls2
 small_files = ["small_{}".format(i) for i in range(1,11)]
 large_files = ["large_{}".format(i) for i in range(1,22)]
 files = small_files + large_files
@@ -28,14 +28,14 @@ for file in files:
             sol_value = float(f.read())
 
         rel_err = (sol_value - avg_value) / sol_value
-        lines.append("{}:\t{:.4f},\t\t{:.4f},\t\t\t{:.2f}\n".format(file, avg_time, avg_value, rel_err))
+        lines.append("{}:\t{:.6f},\t\t{:.2f},\t\t\t{:.4f}\n".format(file, avg_time, avg_value, rel_err))
     else:
         sol_path = "../DATASET/small_scale_solution/{}".format(file)
         with open(sol_path, "r") as f:
             sol_value = float(f.read())
 
         rel_err = (sol_value - avg_value) / sol_value
-        lines.append("{}:\t{:.4f},\t\t\t{:.4f},\t\t\t{:.2f}\n".format(file, avg_time, avg_value, rel_err))
+        lines.append("{}:\t{:.5f},\t\t{:.4f},\t\t\t{:.4f}\n".format(file, avg_time, avg_value, rel_err))
     
     
 
