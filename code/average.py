80f80f7fc6ba5fb6a3c273883a425a43803b458b
san98215 <s-nye@comcast.net>
2024-04-22 11:41:10 -0400

    Added script to compute average of traces

---
+++ code/average.py
@@ -0,0 +1,25 @@
+import glob
+
+alg = "ls2" # Modify for ls1 or ls2
+small_files = ["small_{}".format(i) for i in range(1,11)]
+large_files = ["large_{}".format(i) for i in range(1,22)]
+files = small_files + large_files
+lines = []
+
+for file in files:
+    paths = glob.glob("../output/our_trace_files/{}_{}*".format(file, alg))
+    instances = [path.replace("\\", "/") for path in paths]
+
+    avg_time = 0
+    avg_value = 0
+    for instance in instances:
+        with open(instance, "r") as f:
+            last_line = f.readlines()[-1]
+            time, value = map(float, last_line.split(","))
+            avg_time += time
+            avg_value += value
+
+    lines.append("{}: {}, {}\n".format(file, avg_time/10, avg_value/10))
+
+with open("{}_averages.trace".format(alg), "w") as f:
+    f.writelines(lines)
