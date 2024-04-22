ea5a8b130f80e99aafe529598f7b93eb095ea90f
san98215 <s-nye@comcast.net>
2024-04-22 14:32:56 -0400

    Updated average script

--- code/average.py
+++ code/average.py
@@ -4,7 +4,7 @@ alg = "ls2" # Modify for ls1 or ls2
 small_files = ["small_{}".format(i) for i in range(1,11)]
 large_files = ["large_{}".format(i) for i in range(1,22)]
 files = small_files + large_files
-lines = []
+lines = ["File\t\tTime(s)\t\t\tValue\t\t\t\tRelErr\n"]
 
 for file in files:
     paths = glob.glob("../output/our_trace_files/{}_{}*".format(file, alg))
@@ -19,7 +19,25 @@ for file in files:
             avg_time += time
             avg_value += value
 
-    lines.append("{}:\t{}, {}\n".format(file, avg_time/10, avg_value/10))
+    avg_time /= 10
+    avg_value /= 10
+
+    if "large" in file:
+        sol_path = "../DATASET/large_scale_solution/{}".format(file)
+        with open(sol_path, "r") as f:
+            sol_value = float(f.read())
+
+        rel_err = (sol_value - avg_value) / sol_value
+        lines.append("{}:\t{:.4f},\t\t{:.4f},\t\t\t{:.2f}\n".format(file, avg_time, avg_value, rel_err))
+    else:
+        sol_path = "../DATASET/small_scale_solution/{}".format(file)
+        with open(sol_path, "r") as f:
+            sol_value = float(f.read())
+
+        rel_err = (sol_value - avg_value) / sol_value
+        lines.append("{}:\t{:.4f},\t\t\t{:.4f},\t\t\t{:.2f}\n".format(file, avg_time, avg_value, rel_err))
+    
+    
 
 with open("{}_averages.trace".format(alg), "w") as f:
     f.writelines(lines)
